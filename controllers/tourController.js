c3b7d113436af847955e01b4f29c01c4aeb2f4a0
shreyanshjha <shreyanshjha22@gmail.com>
2025-02-08 11:05:50 +0530

    improvements

---
+++ controllers/tourController.js
@@ -0,0 +1,53 @@
+const Tour = require('./../models/tourModel');
+
+exports.checkBody = (req, res, next) => {
+    if(!req.body.name || !req.body.price){
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or price'
+        });
+    }
+    next();
+}
+exports.getAllTours = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        requestAt: req.requestTime,
+        // results: tours.length,
+        // data: {
+        //     tours
+        // }
+    });
+}
+exports.getTour = (req, res) => {
+    const id = Number(req.params.id);
+    // const tour = tours.find(el => el.id === id);
+    // res.status(200).json({
+    //     status: 'success',
+    //     data: {
+    //         tour
+    //     }
+    // });
+}
+exports.createTour = (req, res) => {
+    res.status(201).json({
+        status: 'success',
+        data: {
+            tour: newTour
+        }
+    });
+}
+exports.updateTour = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: 'Updated tour here...'
+        }
+    });
+}
+exports.deleteTour = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: null
+    });
+}
