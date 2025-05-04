8c967c06e6fed2427d853f5a3ca7e4f6b24d1e44
shreyanshjha <shreyanshjha22@gmail.com>
2025-05-05 00:49:16 +0530

    sort using moongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,20 +4,54 @@ exports.getAllTours = async (req, res) => {
     try {
         // BUILD QUERY
 
-        // 1) FILTERING
+        // 1A) FILTERING
         const queryObj = {...req.query};
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(el => delete queryObj[el]);
+        const excludedFields = ['pageNum', 'sort', 'pageSize', 'fields'];
+        for(let i = 0; i < excludedFields.length; i++)  {
+            delete queryObj[excludedFields[i]];
+        }
 
-        // 2) ADVANCED FILTERING
+        // 1B) ADVANCED FILTERING
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => '$'+match);
 
         // Eg:- Mongo DB querying data with advanced filtering
         // { duration: { $gte: 5 }, difficulty: 'easy' }
 
-        const query = Tour.find(JSON.parse(queryStr));
+        let query = Tour.find(JSON.parse(queryStr));
 
+        // 2) SORTING
+        if(req.query.sort) { 
+            const sortFields = req.query.sort.split(',');
+            const sortObj = {};
+            for(let i = 0; i < sortFields.length; i++) {
+                const [orderBy, orderDirection] = sortFields[i].split(':');
+                sortObj[orderBy] = String(orderDirection);
+            }
+            console.log(sortObj);
+            query = query.sort(sortObj);
+        } else {
+            query = query.sort({'createdAt': 'desc'});
+        }
+
+        // 3) Field limiting
+        if(req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v');
+        }
+
+        // 4) PAGINATION
+        //pageNum=2&pageSize=20, record 1 to 20 => pageNum 1, record 21 to 40 => pageNum 2, record 41 to 60 is pageNum 3 and so on (Industry standard)
+        const pageNum = Number(req.query.pageNum) || 1;
+        const limit = Number(req.query.pageSize) || 100;
+        const skip = (pageNum - 1) * limit;
+        query = query.skip(skip).limit(limit);
+        if(req.query.pageNum) {
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error("This page does not exist");
+        }
         // moongoose query params code just for learning reference
         // const tours = await Tour.find()
         //      .where('duration')
