d054ba21288bf0025cf03ae4b01a770a4331d6ee
shreyanshjha <shreyanshjha22@gmail.com>
2026-01-04 23:56:36 +0530

    compression depedency error resolved

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
