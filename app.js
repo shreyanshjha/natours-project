03ca0848e725056c64cd8616a2f82566ff626bf8
shreyanshjha <shreyanshjha22@gmail.com>
2026-01-04 23:55:22 +0530

    compression depedency install

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -21,6 +22,14 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(cors());
+
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }));
+
+app.options('*', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -76,7 +85,7 @@ app.use(hpp({
 //     console.log('Hello from the middleware 👋');
 //     next();
 // });
-
+app.use(compression());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   //console.log(req.cookies);
