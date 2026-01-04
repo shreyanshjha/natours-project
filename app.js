164185eeede2a25039bcc94f4e685ea007977e7e
shreyanshjha <shreyanshjha22@gmail.com>
2026-01-04 23:43:59 +0530

    Revert "Prepare app for deploying"
    This reverts commit 27375864d33716c2404fae14336677f421d7b105.

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -77,7 +76,6 @@ app.use(hpp({
 //     console.log('Hello from the middleware 👋');
 //     next();
 // });
-app.use(compression());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
